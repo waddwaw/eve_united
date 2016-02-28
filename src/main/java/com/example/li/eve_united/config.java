32f2016715afa2ed388d2f2645b7c63c74f6edc3
waddwaw <waddwaw@qq.com>
2016-02-28 21:57:15 +0800

    修改配置文件

--- src/main/java/com/example/li/eve_united/config.java
+++ src/main/java/com/example/li/eve_united/config.java
@@ -6,10 +6,10 @@ package com.example.li.eve_united;
 public class config {
     //注册请求地址 GET 方式
     //public static final String URL="http://192.168.56.1:8080/Everyone_united/servlet/";
-   public static final String URL="http://192.168.3.2:8080/Everyone_united/servlet/";
+   public static final String URL="http://120.27.33.15:8080/Everyone_united/servlet/";
     //Socket 连接地址
     //public static final String MYSOCKET="192.168.56.1";
-    public static final String MYSOCKET="192.168.3.2";
+    public static final String MYSOCKET="120.27.33.15";
     //Socket 端口号
     public static final int PROT= 8088;
 }
